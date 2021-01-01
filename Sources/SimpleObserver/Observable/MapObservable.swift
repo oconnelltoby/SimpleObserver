a80aee64645d92ac631f19a78b455443b772d0cd
Toby O'Connell <tobyoconnell@hotmail.com>
2021-01-01 22:52:25 +0000

    Add MapObservable

---
+++ Sources/SimpleObserver/Observable/MapObservable.swift
@@ -0,0 +1,21 @@
+public class MapObservable<Upstream, Output>: Observable where Upstream: Observable {
+    private let transform: (Upstream.Output) -> Output
+    private let upstream: Upstream
+    
+    public init(upstream: Upstream, transform: @escaping (Upstream.Output) -> Output) {
+        self.upstream = upstream
+        self.transform = transform
+    }
+    
+    public func observe(_ action: @escaping (Output) -> Void) -> Cancelable {
+        upstream.observe { output in
+            action(self.transform(output))
+        }
+    }
+}
+
+public extension Observable {
+    func map<Output>(_ transform: @escaping (Self.Output) -> Output) -> MapObservable<Self, Output> {
+        MapObservable(upstream: self, transform: transform)
+    }
+}
---
+++ Tests/SimpleObserverTests/MapObservableTests.swift
@@ -0,0 +1,148 @@
+import XCTest
+import SimpleObserver
+
+final class MapObservableTests: XCTestCase {
+    private var passthroughSubject: PassthroughSubject<Void>!
+
+    override func setUp() {
+        passthroughSubject = PassthroughSubject()
+    }
+    
+    func testMapTransformsOutput() {
+        var output: UUID?
+        let uuid = UUID()
+        let transform = { uuid }
+        
+        let cancelable = passthroughSubject
+            .map(transform)
+            .observe {
+                output = $0
+            }
+        
+        passthroughSubject.send(())
+        XCTAssertEqual(output, uuid)
+        
+        cancelable.cancel()
+    }
+    
+    func testTransformNotCalledIfCancelled() {
+        var transformCount = 0
+        
+        let transform: () -> UUID = {
+            transformCount += 1
+            return UUID()
+        }
+        
+        _ = passthroughSubject
+            .map(transform)
+            .observe { _ in }
+        
+        passthroughSubject.send(())
+        XCTAssertEqual(transformCount, 0)
+    }
+    
+    func testTransformNotCalledAgainIfCancelled() {
+        var transformCount = 0
+        
+        let transform: () -> UUID = {
+            transformCount += 1
+            return UUID()
+        }
+        
+        let cancelable = passthroughSubject
+            .map(transform)
+            .observe { _ in }
+        
+        passthroughSubject.send(())
+        XCTAssertEqual(transformCount, 1)
+        
+        cancelable.cancel()
+        
+        passthroughSubject.send(())
+        XCTAssertEqual(transformCount, 1)
+
+        cancelable.cancel()
+    }
+    
+    func testSendingBeforeObservingDoesNotCallTransform() {
+        var transformCount = 0
+        
+        let transform: () -> UUID = {
+            transformCount += 1
+            return UUID()
+        }
+
+        passthroughSubject.send(())
+        
+        let cancelable = passthroughSubject
+            .map(transform)
+            .observe { _ in }
+        
+        XCTAssertEqual(transformCount, 0)
+                
+        cancelable.cancel()
+    }
+    
+    func testMultipleSendsCallsTransformMultipleTimes() {
+        var transformCount = 0
+        
+        let transform: () -> UUID = {
+            transformCount += 1
+            return UUID()
+        }
+        
+        let cancelable = passthroughSubject
+            .map(transform)
+            .observe { _ in }
+        
+        passthroughSubject.send(())
+        XCTAssertEqual(transformCount, 1)
+                
+        passthroughSubject.send(())
+        XCTAssertEqual(transformCount, 2)
+
+        cancelable.cancel()
+    }
+    
+    func testTransformCallOrderMatchesSendOrder() {
+        let passthroughSubject = PassthroughSubject<Int>()
+        
+        var values = [Int]()
+        
+        let transform: (Int) -> Int = {
+            values.append($0)
+            return $0
+        }
+        
+        let cancelable = passthroughSubject
+            .map(transform)
+            .observe { _ in }
+        
+        passthroughSubject.send(0)
+        XCTAssertEqual(values, [0])
+
+        passthroughSubject.send(1)
+        XCTAssertEqual(values, [0, 1])
+        
+        cancelable.cancel()
+    }
+    
+    func testMapObservableIsDeallocatedWhenUpstreamIsCancelled() {
+        let passthroughSubject = PassthroughSubject<Void>()
+        weak var weakMapObservable: MapObservable<PassthroughSubject<Void>, Void>?
+        var cancelable: Cancelable
+        do {
+            let mapObservable = passthroughSubject
+                .map { _ in  }
+            
+            cancelable = mapObservable
+                .observe { _ in }
+            
+            weakMapObservable = mapObservable
+        }
+        
+        XCTAssertNotNil(weakMapObservable)
+        cancelable.cancel()
+        XCTAssertNil(weakMapObservable)
+    }
+}
